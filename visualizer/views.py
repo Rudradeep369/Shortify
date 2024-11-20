a811829ecc8bececbaf0d0635a334a52ca05bfe5
My-new-Repository <debnathrudradeep@gmail.com>
2024-11-21 01:43:30 +0530

    minor changes

---
+++ visualizer/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+def index(request):
+    return render(request, 'index.html')
+
+
+
+
+# Open api key = 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
